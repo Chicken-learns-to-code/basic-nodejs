5370ddbfbe91d36b1e0c396f274c1b3323d91d11
TranNgocTam-19211TT2494 <ngoctam2303001@gmail.com>
2022-04-19 17:40:05 +0700

    create courses & slug auto

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxlength: 255 },
+    name: { type: String, maxlength: 255, required: true },
+    videoId: { type: String },
+    level: { type: String },
     description: { type: String, maxlength: 600 },
     photo: { type: String, maxlength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+    slug: {type: String, slug: 'name', unique: true}, // unique check name already exists in db no?
+},{
+    timestamps: true,
 });
 
 module.exports =  mongoose.model('Course', Course);
